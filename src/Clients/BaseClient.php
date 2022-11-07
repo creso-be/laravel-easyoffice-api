a495b49fc662ae1b1c3f934fc35c2826ff34538c
Dante Schrauwen <contact@danteschrauwen.be>
2022-11-07 16:25:57 +0100

    KEN-383: Created handler for catching errors

--- src/Clients/BaseClient.php
+++ src/Clients/BaseClient.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Creso\LaravelEasyofficeApi\Clients;
 
 use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Log;
 
 class BaseClient
 {
     public function __construct(protected PendingRequest $httpClient)
     {
+        $this->httpClient->throw([$this, 'handleErrors']);
     }
 
     public function createFilterQuery(string $path, array $filters): string
@@ -25,5 +28,17 @@ class BaseClient
         $queryString = http_build_query($filters->toArray());
 
         return "{$path}?{$queryString}";
+    }
+
+    public function handleErrors(Response $response): void
+    {
+        Log::error("[EO] API call to EasyOffice failed with status code {$response->status()}", [
+            'reasonPhrase' => $response->reason(),
+            'headers' => $response->headers(),
+            'requestUri' => $response->transferStats?->getRequest()?->getUri(),
+            'requestTarget' => $response->transferStats?->getRequest()?->getRequestTarget(),
+        ]);
+
+        abort($response->status(), $response->reason());
     }
 }
